d33492547822563dc35f18283bec9383d2a89bad
Maxence Maireaux <maxence@maireaux.fr>
2024-04-20 14:05:23 +0200

    feat: Add Auth layer

---
+++ server/api/auth/github.get.ts
@@ -0,0 +1,9 @@
+export default oauth.githubEventHandler({
+  config: {
+    emailRequired: true
+  },
+  async onSuccess(event, { user }) {
+    await setUserSession(event, { user })
+    return sendRedirect(event, '/dashboard')
+  }
+})
