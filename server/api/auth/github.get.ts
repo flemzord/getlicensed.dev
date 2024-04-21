279901457daa80e6bb29ca10923dd5e9268b797c
Maxence Maireaux <maxence@maireaux.fr>
2024-04-21 22:31:49 +0200

    feat: Save user in Database

--- server/api/auth/github.get.ts
+++ server/api/auth/github.get.ts
@@ -1,9 +1,34 @@
+import {prisma} from "@/prisma/client";
+
+async function getUserByEmail(email: string) {
+  return await prisma.user.findFirst({ where: { email } });
+}
+
+async function createUser(param: { githubId: string; name: string; email: string }) {
+  return await prisma.user.create({ data: param })
+}
+
 export default oauth.githubEventHandler({
   config: {
     emailRequired: true,
   },
   async onSuccess(event, { user }) {
+    const existingUser = await getUserByEmail(user.email);
+    if (!existingUser) {
+      await createUser({
+        email: user.email,
+        name: user.name,
+        githubId: user.id.toString()
+      });
+    }
+
     await setUserSession(event, { user });
     return sendRedirect(event, "/dashboard");
   },
+  onError(event, error) {
+    console.error('Github OAuth error:', error)
+    return sendRedirect(event, '/')
+  },
 });
+
+
